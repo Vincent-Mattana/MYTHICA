2e65dc479a3299114a84593a35e2643fcb636989
Vincent Mattana <vincentm@explore-ai.net>
2025-08-22 15:01:56 +0200

    Added sprites and more

---
+++ assets/Classic Roguelike/TMX Examples/ascii_plus.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="ascii_plus" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="ascii_plus.png" width="256" height="256"/>
+ <tile id="32">
+  <animation>
+   <frame tileid="32" duration="600"/>
+   <frame tileid="33" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="34">
+  <animation>
+   <frame tileid="34" duration="300"/>
+   <frame tileid="35" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="37">
+  <animation>
+   <frame tileid="37" duration="600"/>
+   <frame tileid="38" duration="600"/>
+  </animation>
+ </tile>
+</tileset>
